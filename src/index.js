0e5e432aef20172b942bbcf5a417e7add77ff1a7
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2020-11-29 16:00:03 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "../src/App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { Route, IndexRoute, Router, browserHistory } from "react-router";
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}></Route>
+  </Router>
 );
+
+ReactDOM.render(router, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
